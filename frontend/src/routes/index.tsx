094d47132c090e033d4e3099f5f1a9e4c1e5238d
Duarte Simão <duartesimo@proton.me>
2026-08-06 15:38:55 +0200

    feat(frontend): add login and register pages

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
 import MainLayout from '../layouts/MainLayout'
 
 export default function AppRoutes() {
@@ -9,6 +11,8 @@ export default function AppRoutes() {
 			<Routes>
 				<Route element={<MainLayout />}>
 					<Route index element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
